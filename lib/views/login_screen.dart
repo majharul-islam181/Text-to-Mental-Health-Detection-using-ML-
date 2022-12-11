bc781622a9735bb6651df2d705e2848564697753
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-12-11 23:59:37 +0600

    update

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -1,3 +1,5 @@
+import 'package:fl_country_code_picker/fl_country_code_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
@@ -5,27 +7,47 @@ import 'package:get/get.dart';
 import '../widgets/green_intro_widget.dart';
 import '../widgets/login_widget.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  
+  final countryPicker = const FlCountryCodePicker();
+
+  CountryCode countryCode = CountryCode(name: "Bangladesh", code: "bd", dialCode: '+88');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: SizedBox(
         width: Get.width,
         height: Get.height,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            greenIntroWidget(),
-            const SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: loginWidget(),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              greenIntroWidget(),
+              const SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: loginWidget(countryCode, () async {
+                  final code = await countryPicker.showPicker(context: context);
+                  if (code!= null) countryCode = code;
+                      
+                   setState(() {
+                     
+                   });
+                 
+                }),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/login_widget.dart
+++ lib/widgets/login_widget.dart
@@ -1,12 +1,14 @@
-// ignore_for_file: prefer_interpolation_to_compose_strings
+// ignore_for_file: prefer_interpolation_to_compose_strings, prefer_const_constructors, use_function_type_syntax_for_parameters, avoid_unnecessary_containers
 
+import 'package:fl_country_code_picker/fl_country_code_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mental/utils/app_constants.dart';
 import 'package:mental/widgets/text_widget.dart';
 
-Widget loginWidget() {
-  return Expanded(
+Widget loginWidget(CountryCode countryCode, Function onCountryChange) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 5),
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -18,7 +20,7 @@ Widget loginWidget() {
             fontSize: 24,
             fontWeight: FontWeight.bold),
         const SizedBox(
-          height: 40,
+          height: 30,
         ),
         Container(
           width: double.infinity,
@@ -32,6 +34,44 @@ Widget loginWidget() {
                 spreadRadius: 5,
                 blurRadius: 25,
               )
+            ],
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                  flex: 2,
+                  child: InkWell(
+                    onTap: () => onCountryChange,
+                    child: Container(
+                      child: Row(
+                        children: [
+                          Container(
+                            child: countryCode.flagImage,
+                          ),
+                          textWidget(text: countryCode.dialCode),
+                          Icon(Icons.keyboard_arrow_down_rounded),
+                        ],
+                      ),
+                    ),
+                  )),
+              Container(
+                width: 1,
+                height: 55,
+                color: Colors.black.withOpacity(0.5),
+              ),
+              Expanded(
+                  flex: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        hintText: "Enter your mobile number",
+                        hintStyle: GoogleFonts.poppins(fontSize: 16),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  )),
             ],
           ),
         ),
